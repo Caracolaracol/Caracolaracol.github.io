ff8c42bd87081c2595cdf2b36c056fa5c7de2463
Agustin Rojas <agus.rojasmolina@gmail.com>
2022-09-18 02:01:58 -0300

    animation

--- js/main.js
+++ js/main.js
@@ -2,11 +2,22 @@ const btnWebDevelopment = document.querySelector('#btn-coding')
 const btnMotionGraphics = document.querySelector('#btn-post')
 const welcomeSection = document.querySelector('.welcome-section')
 const footer = document.querySelector('.footer-show')
-const navBar = document.querySelector('.main-nav-show')
 const main = document.querySelector('.main-container-show')
+const t1 = document.querySelector('.t1')
+const t2 = document.querySelector('.t2')
+const t3 = document.querySelector('.t3')
+const t4 = document.querySelector('.t4')
+const t5 = document.querySelector('.t5')
+const header = document.querySelector('.header')
 btnWebDevelopment.addEventListener('click', function(){
     welcomeSection.style.display = 'none'
     footer.style.display = 'flex'
-    navBar.style.display = 'block'
+    header.style.display = 'flex'
+    t1.style.display = 'block'
+    t2.style.display = 'block'
+    t3.style.display = 'block'
+    t4.style.display = 'block'
+    t5.style.display = 'block'
+
     main.style.display= 'block'
 })
